65e2ec3b197df8616ace8f9583904eaa8b250ab5
Marc Mintel <marc@mintel.me>
2020-04-04 13:50:20 +0700

    chore: fix linting

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -2,32 +2,32 @@ module.exports = {
   plugins: [
     'gatsby-tinacms-json',
     'gatsby-transformer-json',
-    `gatsby-transformer-remark`,
+    'gatsby-transformer-remark',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `src/data`,
-        name: `data`
-      }
+        path: 'src/data',
+        name: 'data',
+      },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `src/pages`,
-        name: `pages`
-      }
+        path: 'src/pages',
+        name: 'pages',
+      },
     },
     {
       resolve: 'gatsby-plugin-tinacms',
       options: {
         sidebar: {
-          position: 'displace'
+          position: 'displace',
         },
         plugins: [
           'gatsby-tinacms-git',
-          'gatsby-tinacms-remark'
-        ]
-      }
-    }
-  ]
-}
+          'gatsby-tinacms-remark',
+        ],
+      },
+    },
+  ],
+};
